6bd3e3813ca4a6a83b8643eb10e9789b83b9572e
Rohit Rohit <rohitsaroha125@gmail.com>
2024-03-26 10:53:30 +0530

    Served html file using path library

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const app = express();
 // app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
 
 app.listen(5000, () => {
   console.log("Server Running");
