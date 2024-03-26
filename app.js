0a05d4bcd6beeaabfcc455054122930f3db67a59
Rohit Rohit <rohitsaroha125@gmail.com>
2024-03-26 10:22:52 +0530

    addition of routes folder

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 const express = require("express");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 // app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="username" /><button type="submit">Submit</button></form>'
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body.username);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello From Express</h1>");
-});
-
-app.use((req, res, next) => {
-  console.log("Middleware running");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(5000, () => {
   console.log("Server Running");
