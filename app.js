fb8c0ff155474105e02d46d5acda9d6855804b80
Rohit Rohit <rohitsaroha125@gmail.com>
2024-04-01 09:06:35 +0530

    downloading pdf file

--- app.js
+++ app.js
@@ -2,39 +2,9 @@ const path = require("path");
 const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const fileRoutes = require("./routes/files");
 const errorControllers = require("./controllers/error");
-const multer = require("multer");
-
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      new Date().toISOString() +
-        "-" +
-        Math.random() * 100000 +
-        "-" +
-        file.originalname
-    );
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/gif"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const uploads = multer({ storage: fileStorage, fileFilter });
+const fs = require("fs");
 
 const app = express();
 
@@ -47,17 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.get("/", (req, res, next) => {
-  res.render("fileUpload");
-});
-
-app.post("/file-upload", uploads.single("fileUpload"), (req, res, next) => {
-  const username = req.body.username;
-  const file = req.file;
-  console.log("Username is ", username, file);
-  res.redirect("/");
-});
-
+app.use(fileRoutes);
 // app.use("/admin", adminRoutes.router);
 // app.use(shopRoutes);
 
---
+++ routes/files.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toISOString() +
+        "-" +
+        Math.random() * 100000 +
+        "-" +
+        file.originalname
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/gif"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const uploads = multer({ storage: fileStorage, fileFilter });
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.render("fileUpload");
+});
+
+router.post("/file-upload", uploads.single("fileUpload"), (req, res, next) => {
+  const username = req.body.username;
+  const file = req.file;
+  console.log("Username is ", username, file);
+  res.redirect("/");
+});
+
+router.get("/invoice", (req, res, next) => {
+  res.render("invoiceFile");
+});
+
+router.get("/order/:orderId", (req, res, next) => {
+  const invoicePath = path.join(__dirname, "..", "uploads", "pdf", "blank.pdf");
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+    console.log("data is ", data);
+    res.setHeader("content-type", "application/pdf");
+    res.send(data);
+  });
+});
+
+module.exports = router;
